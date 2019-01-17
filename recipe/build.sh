f94de035ff3a03fc11603ea2fa7604e91bfbb4bc
Santiago Rodriguez <rodri324@purdue.edu>
2019-01-17 10:41:43 -0600

    Update build.sh
    removed flags from cmake build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,7 @@ cmake ..  \
       -DMP_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DBUILD_ASL=ON
+      -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 make -j$CPU_COUNT
 make install
