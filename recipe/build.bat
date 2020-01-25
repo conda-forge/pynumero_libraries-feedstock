5b4f1fff6f724731657d1dca9f1145c08b480d7b
Santiago <santiagoropb@gmail.com>
2020-01-25 11:57:50 -0600

    remove comments

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,10 +1,8 @@
 setlocal EnableDelayedExpansion
 
-:: Make a build folder and change to it.
 mkdir build
 cd build
 
-:: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=$PREFIX ^
@@ -12,10 +10,8 @@ cmake -G "NMake Makefiles" ^
       
 if errorlevel 1 exit 1
 
-:: Build!
 nmake
 if errorlevel 1 exit 1
 
-:: Install!
 nmake install
 if errorlevel 1 exit 1
