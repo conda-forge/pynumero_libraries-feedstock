70abf0185669af3d8ed4f98234a817ddca081a49
Santiago <santiagoropb@gmail.com>
2020-01-25 13:23:05 -0600

    add windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+setlocal EnableDelayedExpansion
+
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DBUILD_ASL=ON ^
+      -DCMAKE_INSTALL_PREFIX=$PREFIX ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
